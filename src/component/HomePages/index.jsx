528e58aef3dd28e16cc13f580d6e171afe2f7f35
khalil <alizadeh3054@gmail.com>
2023-12-04 12:24:08 +0330

    tesst

--- src/component/HomePages/index.jsx
+++ src/component/HomePages/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function HomePages() {
+    const navigate=useNavigate()
   return (
     <div>HomePages555555555555555555555555
-
+<button style={{width:"80px", height:"40px" , backgroundColor:"red"}} onClick={()=>navigate("/home")}>go</button>
     </div>
   )
 }
