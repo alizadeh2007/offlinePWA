ff27837f98db08510e1c4fcd139cdfe8a1e819e9
khalil <alizadeh3054@gmail.com>
2023-12-04 12:26:52 +0330

    tesst

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,6 +4,8 @@ const CACHE_NAME = "Static_1";
 const precachedResources = [
   "/offlinePWA/",
   "/offlinePWA/index.html",
+  "/static/media/circles.fbbfe33606835357a1d59a4f67096356.svg",
+  "/static/media/three-dots.d495cf010a1763f1c105db06e1baa5c8.svg"
 ];
 
 //! Cache
