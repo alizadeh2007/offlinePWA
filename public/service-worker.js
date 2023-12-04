96ae043294814f36a14a93eb7b8c2fe38c14d382
khalil <alizadeh3054@gmail.com>
2023-12-04 13:54:59 +0330

    dsad

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,8 +10,8 @@ const precachedResources = [
   "/offlinePWA/static/media/play.9a6005b5efd5bf1592a599cbb2909e97.svg",
   "/offlinePWA/static/media/Result.54943f038ccfe6c7731aab221e3f16fb.svg",
   "/offlinePWA/static/media/ResultGermany.b3ad8bea018c29aae887f812efe2b944.svg",
-  // "/offlinePWA/static/media/selectSex.176a94a80b9f25b1e4dc7be32a6f4fb5.svg",
-  // "/offlinePWA/static/media/three-dots.d495cf010a1763f1c105db06e1baa5c8.svg",
+  "/offlinePWA/static/media/selectSex.176a94a80b9f25b1e4dc7be32a6f4fb5.svg",
+  "/offlinePWA/static/media/three-dots.d495cf010a1763f1c105db06e1baa5c8.svg",
   "/offlinePWA/static/media/YourMuscleAge.aee3bfd345144de1bce957bb54441b5d.svg",
   "/offlinePWA/",
   "/offlinePWA/index.html",
@@ -38,7 +38,7 @@ self.addEventListener("fetch", (event) => {
         }
 
         //! Fetch the resource and cache it
-        return fetch(request, { mode: "no-cors" }).then(
+        return fetch(request).then(
           (fetchResponse) => {
             return caches.open(CACHE_NAME).then((cache) => {
               cache.put(request, fetchResponse.clone());
