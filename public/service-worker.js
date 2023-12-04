fd06c921a4d71ae6a4681ad61ad6eaf83ed23ec8
khalil <alizadeh3054@gmail.com>
2023-12-04 12:42:13 +0330

    dsad

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-restricted-globals */
 //! Static caches 
-const CACHE_NAME = "Static_2";
+const CACHE_NAME = "Static_3";
 const precachedResources = [
-  "/",
-  "/index.html",
+  "/offlinePWA/",
+  "/offlinePWA/index.html",
 ];
 
 //! Cache
