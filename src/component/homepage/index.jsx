f7547659198170273b39feda137972e098958acc
khalil <alizadeh3054@gmail.com>
2023-12-04 12:19:34 +0330

    tesst

--- src/component/homepage/index.jsx
+++ src/component/homepage/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Icon } from '@iconify/react';
+import iconss from "./../../New folder/circles.svg"
+import iconss2 from "./../../New folder/three-dots.svg"
 
 function Home() {
   return (
     <div>Home
     <Icon icon="material-symbols:1k-plus-sharp" />
     <Icon icon="material-symbols:18mp-outline-sharp" />
-    
+    <Icon icon="grommet-icons:apple" />
+    <img src={iconss}/>
+    <img src={iconss2}/>
     </div>
   )
 }
