402dec7be8b386a5045ffe1fb4fd08ed00ce0a70
khalil <alizadeh3054@gmail.com>
2023-12-04 13:59:16 +0330

    Updates

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@ const precachedResources = [
   "/offlinePWA/static/media/play.9a6005b5efd5bf1592a599cbb2909e97.svg",
   "/offlinePWA/static/media/Result.54943f038ccfe6c7731aab221e3f16fb.svg",
   "/offlinePWA/static/media/ResultGermany.b3ad8bea018c29aae887f812efe2b944.svg",
-  "/offlinePWA/static/media/selectSex.176a94a80b9f25b1e4dc7be32a6f4fb5.svg",
+  // "/offlinePWA/static/media/selectSex.176a94a80b9f25b1e4dc7be32a6f4fb5.svg",
   "/offlinePWA/static/media/YourMuscleAge.aee3bfd345144de1bce957bb54441b5d.svg",
   "/offlinePWA/",
   "/offlinePWA/index.html",
