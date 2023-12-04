97b70d22e5668cacfeaa9e65fe0dcc5912a40432
khalil <alizadeh3054@gmail.com>
2023-12-04 12:07:45 +0330

    Third comments

--- src/component/HomePages/index.jsx
+++ src/component/HomePages/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { Icon } from '@iconify/react';
 
 function HomePages() {
   return (
     <div>HomePages555555555555555555555555
-    <Icon icon="material-symbols:1k-plus-sharp" />
-    <Icon icon="material-symbols:18mp-outline-sharp" />
+
     </div>
   )
 }
--- src/component/homepage/index.jsx
+++ src/component/homepage/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Icon } from '@iconify/react';
 
 function Home() {
   return (
     <div>Home
-    
+    <Icon icon="material-symbols:1k-plus-sharp" />
+    <Icon icon="material-symbols:18mp-outline-sharp" />
     
     </div>
   )
