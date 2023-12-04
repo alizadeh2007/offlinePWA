5a9aaf6621e0ea0c19ee285ebf3624b32e4acfcc
khalil <alizadeh3054@gmail.com>
2023-12-04 11:23:04 +0330

    Third comments

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,3 +13,45 @@ self.addEventListener("install", (event) => {
       return cache.addAll(precachedResources);
    })());
 });
+
+//! Fetch and Updating | Dynamicy
+self.addEventListener("fetch", (event) => {
+  const request= event.request
+
+  if (event.request.method !== "POST") {
+    event.respondWith(
+      caches.match(request).then( response => {
+        // Return the cached response if found
+        if (response) {
+          return response
+        }
+
+        //! Fetch the resource and cache it
+        return fetch(request, { mode: "no-cors" }).then(
+          (fetchResponse) => {
+            return caches.open(CACHE_NAME).then((cache) => {
+              cache.put(request, fetchResponse.clone());
+              return fetchResponse;
+            });
+          }
+        );
+      })
+    );
+  }
+});
+
+//! Activate 
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
