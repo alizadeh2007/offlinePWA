4d0feb55a0e770b18840155e6e3101e61a8c8a07
khalil <alizadeh3054@gmail.com>
2023-12-04 10:31:48 +0330

    Third comments

---
+++ public/service-worker.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-restricted-globals */
+//! Static caches 
+const CACHE_NAME = "Static_4";
+const precachedResources = [
+  '/static/media/logo.103b5fa18196d5665a7e12318285c916.svg',
+  "/",
+  "/index.html",
+];
+
+//! Cache
+self.addEventListener("install", (event) => {
+   event.waitUntil((async () => {
+      const cache = await caches.open(CACHE_NAME);
+      return cache.addAll(precachedResources);
+   })());
+});
+
+//! Fetch and Updating | Dynamicy
+self.addEventListener("fetch", (event) => {
+  const request= event.request
+
+  if (event.request.method !== "POST") {
+    event.respondWith(
+      caches.match(request).then( response => {
+        // Return the cached response if found
+        if (response) {
+          return response
+        }
+
+        //! Fetch the resource and cache it
+        return fetch(request, { mode: "no-cors" }).then(
+          (fetchResponse) => {
+            return caches.open(CACHE_NAME).then((cache) => {
+              cache.put(request, fetchResponse.clone());
+              return fetchResponse;
+            });
+          }
+        );
+      })
+    );
+  }
+});
+
+//! Activate 
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
