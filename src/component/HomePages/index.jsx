d637f064da3e5ae1cc2d38e8040b7ee12bc7c0c0
khalil <alizadeh3054@gmail.com>
2023-12-05 12:36:52 +0330

    test

--- src/component/HomePages/index.jsx
+++ src/component/HomePages/index.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 function HomePages() {
     const navigate=useNavigate()
   return (
-    <div style={{backgroundColor:"green", width:"100%" ,height:"100%"}}>
+    <div style={{backgroundColor:"black", width:"100%" ,height:"100%",color:"white"}}>
     some icons at the next page. i want to cache in this page befove navigate to next(with go button)
 <button style={{width:"80px", height:"40px" , backgroundColor:"red"}} onClick={()=>navigate("/home")}>go</button>
     </div>
