f5b7ac794c345c6f7b9f1ed54d7c69c8e9b96dce
khalil <alizadeh3054@gmail.com>
2023-12-05 12:58:46 +0330

    test

--- public/service-worker.js
+++ public/service-worker.js
@@ -27,29 +27,31 @@ self.addEventListener("install", (event) => {
 });
 
 //! Fetch and Updating | Dynamicy
-self.addEventListener("fetch", (event) => {
-  const request= event.request
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
 
-  if (event.request.method !== "POST") {
-    event.respondWith(
-      caches.match(request).then( response => {
-        // Return the cached response if found
-        if (response) {
-          return response
-        }
-
-        //! Fetch the resource and cache it
-        return fetch(request).then(
-          (fetchResponse) => {
-            return caches.open(CACHE_NAME).then((cache) => {
-              cache.put(request, fetchResponse.clone());
-              return fetchResponse;
-            });
-          }
-        );
-      })
-    );
+  if (request.method !== 'POST' && request.url.startsWith('chrome-extension://')) {
+    // Ignore requests with the 'chrome-extension' scheme
+    return;
   }
+
+  // Handle other requests as usual
+  event.respondWith(
+    caches.match(request).then((response) => {
+      // Return the cached response if found
+      if (response) {
+        return response;
+      }
+
+      // Fetch the resource and cache it
+      return fetch(request).then((fetchResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    })
+  );
 });
 
 //! Activate 
